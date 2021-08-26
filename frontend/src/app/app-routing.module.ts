d2796e8e61782c978f9dae148de4cf1cd0703766
Jaime Castro <jandrescastro97@gmail.com>
2021-08-26 12:02:35 -0500

    add frontend

---
+++ frontend/src/app/app-routing.module.ts
@@ -0,0 +1,42 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { RegisterStudentComponent } from './home/register-student/register-student.component';
+import { RegisterTeacherComponent } from './home/register-teacher/register-teacher.component';
+import { RegisterMatterComponent } from './home/register-matter/register-matter.component';
+import { RegisterCourseComponent } from './home/register-course/register-course.component';
+import { LoginComponent } from './home/login/login.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: RegisterStudentComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: 'registerStudent',
+    component: RegisterStudentComponent,
+  },
+  {
+    path: 'registerTeacher',
+    component: RegisterTeacherComponent,
+  },
+  {
+    path: 'registerMatter',
+    component: RegisterMatterComponent,
+  },
+  {
+    path: 'registerCourse',
+    component: RegisterCourseComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  ];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
